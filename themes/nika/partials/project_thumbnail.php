8b5f9347651e236dceb0d6bc76f40c8e047bb738
dylanfisher <hi@dylanfisher.com>
2015-05-17 21:52:43 -0400

    thumbnail class

--- themes/nika/partials/project_thumbnail.php
+++ themes/nika/partials/project_thumbnail.php
@@ -1,9 +1,9 @@
-<div class="project-thumbnail neutral">
+<?php $max_width = get_field('featured_image_size') ? get_field('featured_image_size') : '80'; ?>
+<?php $full_width_class = $max_width == '100' ? 'full-width' : ''; ?>
+<div class="project-thumbnail neutral <?php echo $full_width_class; ?>">
   <a class="project-thumbnail-content" href="<?php the_permalink(); ?>">
     <div class="cover accent-background"></div>
-    <?php $max_width = get_field('featured_image_size') ? get_field('featured_image_size') : '80'; ?>
-    <?php $full_width_class = $max_width == '100' ? 'full-width' : ''; ?>
-    <div class="project-thumbnail-image <?php echo $full_width_class; ?>" style="max-width: <?php echo $max_width; ?>%;">
+    <div class="project-thumbnail-image" style="max-width: <?php echo $max_width; ?>%;">
       <?php sandbox_image('featured_image', 'medium'); ?>
     </div>
   </a>
