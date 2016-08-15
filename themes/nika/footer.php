c71fcb6cb8a580e8619975c424b8f002597c7fff
dylanfisher <hi@dylanfisher.com>
2016-08-15 12:50:26 -0400

    Bust css and js cache

--- themes/nika/footer.php
+++ themes/nika/footer.php
@@ -1,5 +1,5 @@
 </div><!-- .wrapper -->
-<script src="<?php echo get_bloginfo('template_url'); ?>/js/build/application.min.js"></script>
+<script src="<?php echo get_bloginfo('template_url'); ?>/js/build/application.min.js?v2"></script>
 <script>
   (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
   function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
